986eee5de659bce0c75406666b8c94699cb83314
cmrivera <cmrivera0004@student.stcc.edu>
2020-06-08 17:48:35 -0400

    finished with all styling and api pulling

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,57 +1,75 @@
 import React from "react";
 import "./App.css";
 import axios from "axios";
+import Card from "./components/userCard";
+
+//import UserCard from "./components/userCard";
 
 class App extends React.Component {
-  state = {
-    users: [],
-    followers: [],
-  };
+  constructor() {
+    super();
 
-  componentDidMount() {
-    console.log("componentDidMount running");
-    axios
-      .get("https://api.github.com/users/cmrivera")
-      .then((res) => {
-        console.log(res);
-        this.setState({ users: res.data.message });
-      })
-      .catch((err) => console.log(err));
+    this.state = {
+      users: "",
+      followers: [],
+    };
   }
-  handleChanges = (e) => {
+  componentDidMount() {
+    axios.get("https://api.github.com/users/cmrivera").then((res1) => {
+      console.log(res1.data);
+      axios
+        .get(`https://api.github.com/users/cmrivera/followers`)
+        .then((res2) => {
+          console.log(res2.data);
+          this.setState({
+            users: res1.data,
+            followers: res2.data,
+          });
+        });
+    });
+  }
+  /*handleChanges = (e) => {
     console.log(e.target.name, e.target.value);
-    this.setState({ ...this.state, followers: e.target.value });
-  };
+    this.setState({ ...this.state, user: e.target.value });
+  };*/
 
-  fetchUsers = (e) => {
+  /*fetchUsers = (e) => {
     e.preventDefault();
     axios
-      .get(`https://api.github.com/users/cmrivera`)
-      .then((res) => this.setState({ ...this.state, users: res.data.message }))
+      .get(`https://api.github.com/users/cmrivera/followers`)
+      .then((res) =>
+        this.setState({ ...this.state, followers: res.data.followers_url })
+      )
       .catch((err) => console.log(err));
   };
-
+*/
   render() {
     console.log("rendering");
     return (
       <div className="App">
-        <header className="App-header">
-          <h1> Chris's Github UserCard App</h1>
-        </header>
-        <form onSubmit={this.fetchUsers}>
-          <input
-            type="text"
-            name="users"
-            value={this.state.followers}
-            onChange={this.handleChanges}
-          />
-          <button onClick={this.fetchUsers}> Fetch Users</button>
-          <div className="users">
-            {this.state.followers.map((follower) => (
-              <li src={follower} key={follower} />
-            ))}
-          </div>
-        </form>
+        <h1>Chris's React Github UserCard App</h1>
+        <Card
+          userImg={this.state.users.avatar_url}
+          name={this.state.users.name}
+          username={this.state.users.login}
+          profile={this.state.users.url}
+          followers={this.state.users.followers}
+          following={this.state.users.following}
+          bio={this.state.users.bio}
+        />
+        {this.state.followers.map((user) => {
+          return (
+            <Card
+              userImg={user.avatar_url}
+              name={user.name}
+              username={user.login}
+              profile={user.url}
+              followers={user.followers}
+              following={user.following}
+              bio={user.bio}
+            />
+          );
+        })}
       </div>
     );
   }
--- github-user-card/src/components/userData.js
+++
@@ -1,3 +0,0 @@
-import react from "react";
-
-class UserData extends React.Component {}
